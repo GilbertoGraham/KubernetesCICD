7ce50f57adcc08a971e33a1cf1d0012b57b6525b
root <root@Ansible-Controller.o3qvbs0qgrmebpjwnl5akkkdxc.gx.internal.cloudapp.net>
2019-08-06 19:14:36 +0000

    test CI/CD

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 9000;
 app.get('/', (req, res) => {
-    res.send('<h1>Simple node APP - Gilberto Graham je</h1>');
+    res.send('<h1>Simple node APP - Gilberto Graham -- Commit success --</h1>');
 });
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
